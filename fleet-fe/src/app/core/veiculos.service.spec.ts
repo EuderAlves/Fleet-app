fa4b54659984ec585009bb1817be686fd39e4a3c
Euder Alves <euder.martinsalves@emeal.nttdata.com>
2024-05-17 17:12:11 -0300

    cobertura de testes 100% para o serviceVeiculos

--- fleet-fe/src/app/core/veiculos.service.spec.ts
+++ fleet-fe/src/app/core/veiculos.service.spec.ts
@@ -1,16 +1,69 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { VeiculosService } from './veiculos.service';
+import { Veiculos } from '../model/veiculos.model';
+import { catchError, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+const mockHttpClient = jasmine.createSpyObj('HttpClient', ['get', 'post', 'delete']);
 
 describe('VeiculosService', () => {
   let service: VeiculosService;
+  let httpClient: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        VeiculosService,
+        { provide: HttpClient, useValue: mockHttpClient },
+      ],
+    });
     service = TestBed.inject(VeiculosService);
+    httpClient = TestBed.inject(HttpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return a list of vehicles (success)', () => {
+
+    expect(service.listarVeituclos()).toBeUndefined();
+
+  });
+
+  it('should create a new vehicle (success)', () => {
+    const newVeiculo: Veiculos = {
+      id: 2, marca: 'Toyota', modelo: 'Corolla',
+      versao: '',
+      anoFabricacao: 0,
+      anoModelo: 0,
+      cor: '',
+      blindado: false,
+      kilometragem: 0,
+      placa: '',
+      renavam: 0
+    };
+    expect(service.salvarNovoVeiculo(newVeiculo)).toBeUndefined();
+  });
+
+
+
+ it("should delete vehicle", ()=>{
+  const newVeiculo: Veiculos = {
+    id: 2, marca: 'Toyota', modelo: 'Corolla',
+    versao: '',
+    anoFabricacao: 0,
+    anoModelo: 0,
+    cor: '',
+    blindado: false,
+    kilometragem: 0,
+    placa: '',
+    renavam: 0
+  };
+  expect(service.delete(newVeiculo.id)).toBeUndefined();
+ })
 });
