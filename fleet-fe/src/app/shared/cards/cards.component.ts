d7b318e023b93c06d69cc165ef98f4d4ec7c88c4
Euder Alves <euder.martinsalves@emeal.nttdata.com>
2024-05-16 22:05:49 -0300

    adicionado bootstrap e finalizado layout cards

--- fleet-fe/src/app/shared/cards/cards.component.ts
+++ fleet-fe/src/app/shared/cards/cards.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.scss']
 })
-export class CardsComponent {
+export class CardsComponent implements OnInit {
+@Input() marca:string =""
+@Input() modelo:string =""
+@Input() versao:string =""
+@Input() anoFabricacao:string =""
+@Input() anoModelo:string =""
+@Input() cor:string =""
+@Input() blindado:string =""
+@Input() kilometragem:string =""
+@Input() placa:string =""
+@Input() renavam:string =""
+@Input() id:string =""
 
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
+  remover(id:string){
+
+  }
 }
