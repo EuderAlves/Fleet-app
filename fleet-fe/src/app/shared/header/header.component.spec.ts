2a4dd7dfbe5bc8b54170b1ac176fe322b8f09a16
Euder Alves <euder.martinsalves@emeal.nttdata.com>
2024-05-17 22:24:52 -0300

    header teste 100%

--- fleet-fe/src/app/shared/header/header.component.spec.ts
+++ fleet-fe/src/app/shared/header/header.component.spec.ts
@@ -1,15 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
       declarations: [HeaderComponent]
-    });
+    }).compileComponents();
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
