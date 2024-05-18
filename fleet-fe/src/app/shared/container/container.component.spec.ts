428425fbd2c9e59259998aebdf49367ced8eeda0
Euder Alves <euder.martinsalves@emeal.nttdata.com>
2024-05-17 22:29:06 -0300

    container teste 100%

--- fleet-fe/src/app/shared/container/container.component.spec.ts
+++ fleet-fe/src/app/shared/container/container.component.spec.ts
@@ -1,15 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContainerComponent } from './container.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('ContainerComponent', () => {
   let component: ContainerComponent;
   let fixture: ComponentFixture<ContainerComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
       declarations: [ContainerComponent]
-    });
+    }).compileComponents();
     fixture = TestBed.createComponent(ContainerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- fleet-fe/src/app/shared/modal/modal.component.spec.ts
+++ fleet-fe/src/app/shared/modal/modal.component.spec.ts
@@ -1,15 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ModalComponent } from './modal.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('ModalComponent', () => {
   let component: ModalComponent;
   let fixture: ComponentFixture<ModalComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
       declarations: [ModalComponent]
-    });
+    }).compileComponents();
     fixture = TestBed.createComponent(ModalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
